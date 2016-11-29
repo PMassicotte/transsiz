22dda497701b5d72d19807b60362e577965549bd
Philippe Massicotte <pmassicotte@hotmail.com>
2016-11-29 14:13:04 -0500

    update the calculation

--- R/main.R
+++ R/main.R
@@ -10,7 +10,7 @@ loadfonts(quiet = TRUE)
 theme_set(theme_bw(base_size = 12, base_family = "Open Sans"))
 
 ## Do the calculation and generate the report
-ezknitr::ezspin("R/pp.R", out_dir = "reports/", keep_html = FALSE, chunk_opts = list(fig.width = 10))
+ezknitr::ezspin("R/pp.R", out_dir = "reports/", keep_html = FALSE, chunk_opts = list(tidy = TRUE))
 
 ## Publish it on github
 system("sh ./publish.sh")
