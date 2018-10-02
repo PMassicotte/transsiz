2d523a8a55b5ff3bf7e917e4008e67d523a0e7e5
Philippe Massicotte <pmassicotte@hotmail.com>
2018-10-02 13:50:56 -0400

    Fig2 <-> Fig3

--- R/fig3.R
+++ R/fig3.R
@@ -3,20 +3,10 @@
 #
 # DESCRIPTION:  
 #
-# Figure comparing mean vertical profils of primary production.
+# Primary production calculated from suit rov transmittances.
 # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 rm(list = ls())
-
-geomean <- function(x, na.rm = FALSE, trim = 0, ...)
-{
-  exp(mean(log(x, ...), na.rm = na.rm, trim = trim, ...))
-}
-
-geosd <- function(x, na.rm = FALSE, ...)
-{
-  exp(sd(log(x, ...), na.rm = na.rm, ...))
-}
 
 # PAR data ----------------------------------------------------------------
 
@@ -32,7 +22,7 @@ suit_no_lead <- read_feather("data/clean/suit_propagated_par_water_column.feathe
 rov <- read_feather("data/clean/rov_propagated_par_water_column.feather") %>%
   mutate(data_source = "rov")
 
-df <- bind_rows(suit, rov, suit_no_lead) %>%
+df <- bind_rows(suit, suit_no_lead, rov) %>%
   select(
     id,
     data_source,
@@ -42,10 +32,8 @@ df <- bind_rows(suit, rov, suit_no_lead) %>%
     depth,
     par_z_variable_transmittance,
     par_z_100_percent_transmittance
-  )
-
-# %>%
-#   filter(station != 32) ## Station 32 is discarded because there are no matching ROV-SUIT pairs.
+  ) %>%
+  filter(station != 32) ## Station 32 is discarded because there are no matching ROV-SUIT pairs.
 
 ## At the end, shall we use averaged PAR profiles? If so we will loose
 ## information on the variability induced by transmittance measurements.
@@ -57,90 +45,98 @@ df <- df %>%
   left_join(pvse, by = c("station", "depth")) %>% 
   select(-contains("cast"))
 
-## Daily integration
-# df %>%
-#   mutate(pp_under_ice = ps * (1 - exp(-alpha * par_z_variable_transmittance / ps))) %>%
-#   mutate(pp_open_water = ps * (1 - exp(-alpha * par_z_100_percent_transmittance / ps))) %>%
-#   group_by(data_source, station, depth) %>%
-#   nest() %>%
-#   mutate(depth_integrated_pp_under_ice = map_dbl(data, ~pracma::trapz(.$hour, .$pp_under_ice))) %>%
-#   mutate(depth_integrated_pp_open_water = map_dbl(data, ~pracma::trapz(.$hour, .$pp_open_water)))
+# Calulate PP -------------------------------------------------------------
 
-## Daily integration at each depth
+# df <- df %>% 
+#   filter(data_source == "suit")
+
 res <- df %>%
-  filter(station %in% c(19, 27, 31, 39, 43, 46, 47)) %>%
   mutate(pp_under_ice = ps * (1 - exp(-alpha * par_z_variable_transmittance / ps))) %>%
   mutate(pp_open_water = ps * (1 - exp(-alpha * par_z_100_percent_transmittance / ps))) %>%
-  group_by(data_source, station, depth, hour) %>%
-  summarise(
-    mean_pp_under_ice = mean(pp_under_ice),
-    mean_pp_open_water = mean(pp_open_water),
-    sd_pp_under_ice = sd(pp_under_ice)
-  ) %>% 
-  group_by(data_source, station, depth) %>% 
-  summarise(
-    pp_under_ice = pracma::trapz(hour, mean_pp_under_ice),
-    pp_open_water = pracma::trapz(hour, mean_pp_open_water)
-  ) %>%
-  ungroup()
+  group_by(id, data_source, station, hour) %>%
+  nest() %>%
+  mutate(depth_integrated_pp_under_ice = map_dbl(data, ~ pracma::trapz(.$depth, .$pp_under_ice))) %>%
+  mutate(depth_integrated_pp_open_water = map_dbl(data, ~ pracma::trapz(.$depth, .$pp_open_water))) %>%
+  mutate(id = rep(1:((nrow(.)) / 24), each = 24)) %>% 
+  group_by(id, station, data_source) %>% 
+  nest() %>% 
+  mutate(daily_integrated_pp_under_ice = map_dbl(data, ~pracma::trapz(.$hour, .$depth_integrated_pp_under_ice))) %>% 
+  mutate(daily_integrated_pp_open_water = map_dbl(data, ~pracma::trapz(.$hour, .$depth_integrated_pp_open_water))) %>% 
+  select(-data)
 
-res
+# SIC ---------------------------------------------------------------------
+
+## Match with SIC data (used to the mixing model)
 
 sic <- read_csv("data/clean/sic.csv") %>% 
   select(-sd)
 
-res <- res %>% 
-  left_join(sic) %>% 
+res  <- res %>% 
+  left_join(sic)
+
+
+# Mixing model ------------------------------------------------------------
+
+res <- res %>%
   mutate(
-    pp_mixing_model = ((sic_9) * pp_under_ice) + ((1 - sic_9) * pp_open_water)
-  ) 
+    daily_integrated_pp_mixing_model = ((sic_9) * daily_integrated_pp_under_ice) + ((1 - sic_9) * daily_integrated_pp_open_water)
+  )
 
 ## Remove open water pp, we do not need it now
 res <- res %>% 
-  select(-pp_open_water)
+  select(-daily_integrated_pp_open_water)
+
 
 # Remove non-needed data --------------------------------------------------
 
 df <- res %>% 
-  tidyr::gather(pp_source, pp, starts_with("pp_"))
+  tidyr::gather(pp_source, pp, starts_with("daily"))
 
 ## For the SUIT data, we only want to keep:
 ## 1) the mixing model made with transmittance <= 0.1
 ## 2) the pp underice made with all suit data (not with T <= 0.1)
 
 df <- df %>% 
-  filter(!(data_source == "suit" & pp_source == "pp_mixing_model")) %>% 
-  filter(!(data_source == "suit_no_lead" & pp_source == "pp_under_ice")) %>% 
+  filter(!(data_source == "suit" & pp_source == "daily_integrated_pp_mixing_model")) %>% 
+  filter(!(data_source == "suit_no_lead" & pp_source == "daily_integrated_pp_under_ice")) %>% 
   mutate(data_source = if_else(str_detect(data_source, "suit"), "suit", data_source))
-
-# In-situ PP (Jean-Eric) --------------------------------------------------
-
-# jet <-
-#   read_excel(
-#     "data/raw/TRANSSIZ_PP.xlsx",
-#     col_names = c("station", "ctd", "depth", "data_source", "pp"),
-#     range = cell_limits(c(3,1), c(NA, 5))
-#   ) %>% 
-#   mutate(data_source = str_to_lower(data_source)) %>% 
-#   filter(data_source == "in situ") %>% 
-#   select(station, depth, data_source, pp) %>% 
-#   mutate(pp_source = "in situ") %>% 
-#   filter(station %in% c(19, 27, 31, 39, 43, 46, 47)) 
-# 
-# jet
-# 
-# res <- bind_rows(df, jet)
 
 
 # Plot --------------------------------------------------------------------
 
-df %>% 
-  ggplot(aes(x = pp, y = depth, color = interaction(pp_source, data_source))) +
-  geom_path() +
-  scale_y_reverse() +
-  facet_wrap(~station, scales = "free") +
-  xlab(bquote("Primary production" ~(mgC%*%m^{-3}%*%d^{-1}))) +
-  ylab("Depth (m)") +
-  theme(legend.title = element_blank())
+counts <- df %>% 
+  group_by(station, data_source, pp_source) %>% 
+  tally() %>% 
+  ungroup()
+
+plain <- function(x, ...) {
+  format(x, ..., scientific = FALSE, drop0trailing = TRUE)
+}
+
+p <- df %>% 
+  mutate(data_source = str_to_title(data_source)) %>% 
+  ggplot(aes(x = data_source, y = pp, fill = pp_source)) +
+  # geom_boxplot() +
+  geom_violin(scale = "width", size = 0.25) +
+  facet_wrap(~station, scales = "free_y") +
+  scale_y_log10(labels = plain) +
+  annotation_logticks(sides = "l", size = 0.25) +
+  theme(legend.title = element_blank()) +
+  theme(axis.title.x = element_blank()) +
+  scale_fill_discrete(labels = c("daily_integrated_pp_under_ice" = "Under ice", "daily_integrated_pp_mixing_model" = "Mixing model")) +
+  theme(legend.justification = c(0, 0), legend.position = c(0.35, 0.1)) +
+  ylab(bquote("Primary production" ~(mgC%*%m^{-2}%*%d^{-1})))
 
 ggsave("graphs/fig3.pdf", device = cairo_pdf, width = 7, height = 6.22 * 0.75)
+
+write_feather(df, "data/clean/primary_production_rov_vs_suit.feather")
+write_csv(df, "data/clean/primary_production_rov_vs_suit.csv")
+
+# Stats -------------------------------------------------------------------
+
+round(range(df$pp), digits = 3)
+
+df %>% 
+  group_by(station, data_source, pp_source) %>% 
+  summarise(mean_pp = mean(pp)) %>% 
+  arrange(desc(mean_pp))
